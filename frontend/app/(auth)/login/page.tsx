eb5e8f1e5b6e922f142153b9438a4de2e576302e
Artak822 <artak.oga07@gmail.com>
2026-06-03 12:23:52 +0300

    chore: migrate to new repository

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -69,11 +69,7 @@ export default function LoginPage() {
   return (
     <div className={styles.card}>
       <div className={styles.wordmark}>
-        <div className={styles.logos}>
-          <img src="/aspeers.png" alt="АСПиРС" className={styles.logoAspeers} />
-          <div className={styles.logosDivider} />
-          <img src="/sirius.png" alt="Сириус" className={styles.logoSirius} />
-        </div>
+        <img src="/aspeers.png" alt="АСПиРС" className={styles.logoAspeers} />
       </div>
 
       <form className={styles.form} onSubmit={handleSubmit(onSubmit)} noValidate>
